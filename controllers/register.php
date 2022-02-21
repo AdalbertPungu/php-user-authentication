f88fe779ed50660db3863e8192a7240834065167
Adalbert Pungu <18pw419@esisalama.org>
2022-02-21 21:26:56 +0200

    stockage des données récuperer dans des variables

---
+++ controllers/register.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$name = $_POST["nom"];
+$postnom = $_POST["postnom"];
+$prenom = $_POST["prenom"];
+$genre = $_POST["genre"];
+$matricule = $_POST["matricule"];
+$promotion = $_POST["promotion"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$password = $_POST["password"];
+$password2 = $_POST["password2"];
+$adresse = $_POST["adresse"];
+
