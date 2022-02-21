b39cb528364b27fa2b71b63ce93c100a717a61d5
Adalbert Pungu <18pw419@esisalama.org>
2022-02-21 21:38:28 +0200

    vérification des champs

--- controllers/register.php
+++ controllers/register.php
@@ -14,3 +14,47 @@ $password = $_POST["password"];
 $password2 = $_POST["password2"];
 $adresse = $_POST["adresse"];
 
+if ($name == "") {
+    echo "Le nom est vide<br><br>";
+}
+
+if ($postnom == "") {
+    echo "Le postnom est vide<br><br>";
+}
+
+if ($prenom == "") {
+    echo "Le prenom est vide<br><br>";
+}
+
+if ($genre == "") {
+    echo "Le genre  est vide<br><br>";
+}
+
+if ($phone == "") {
+    echo "Le numéro de télephone est vide<br><br>";
+}
+
+if ($matricule == "") {
+    echo "Le matricule est vide<br><br>";
+}
+
+if ($promotion == "") {
+    echo "La promotion est vide<br><br>";
+}
+
+if ($email == "") {
+    echo "L'email  est vide<br><br>";
+}
+
+if ($password == "") {
+    echo "Le mot de passe est vide<br><br>";
+}
+
+if ($password2 == "") {
+    echo "Le mot de passe est vide<br><br>";
+}
+
+if ($adresse == "") {
+    echo "L'adresse est vide<br><br>";
+}
+
