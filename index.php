c79083fcd20ea306e00c443f90cb74c110b15942
Adalbert Pungu <18pw419@esisalama.org>
2022-02-21 20:55:03 +0200

    Add Structure of the html page in index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    
+    session_start();
+    
+    if (isset($_SESSION['connexion']) && !empty($_SESSION['connexion'])) {
+        header('Location: ./profile.php');
+    }
+
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Enregistrement</title>
+    </head>
+    <body>
+
+    </body>
+</html>
