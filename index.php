5ce61d5cde6592ff22792be2bc078d05582f3116
Adalbert Pungu <18pw419@esisalama.org>
2022-02-21 21:20:11 +0200

    add the body

--- index.php
+++ index.php
@@ -16,6 +16,44 @@
         <title>Enregistrement</title>
     </head>
     <body>
+        <h1>Formulaire d'enregistrement</h1><hr><br>
+        <form action="/controllers/register.php" method="post">
 
+            Nom : <br><input type="text" placeholder="Nom" name="nom"><br><br>
+            
+            Postnom : <br><input type="text" placeholder="Postnom" name="postnom"><br><br>
+            
+            Prenom : <br><input type="text" placeholder="Prenom" name="prenom"><br><br>
+            
+            Matricule : <br><input type="text" placeholder="Matricule" name="matricule"><br><br>
+            
+            Email : <br><input type="email" placeholder="Email" name="email"> <?= (isset($_GET['error'])) ? $_GET['error'] : "" ?><br><br>
+            
+            Phone : <br><input type="tel" placeholder="Téléphone" name="phone"><br><br>
+            
+            Mot de passe : <br><input type="password" placeholder="Mot de passe" name="password"><br><br>
+            
+            Confirmer : <br><input type="password" placeholder="Confirmé" name="password2"><br><br>
+            
+            Adresse :  <br><input type="text" placeholder="Adresse" name="adresse"><br><br>
+            
+            Genre :
+            <select name="genre">
+                <option value="Masculin">Masculin</option>
+                <option value="Féminin">Féminin</option>
+            </select><br><br>
+            
+            Promotion :
+            <select name="promotion">
+                <option value="Prepa">Prepa</option>
+                <option value="L1">L1</option>
+                <option value="L2">L2</option>
+                <option value="L3">L3</option>
+            </select><br><br>
+            
+            <input type="submit" value="S'enregistrer">
+        
+        </form><br>
+        <a href="login.php">Se connecter</a>
     </body>
 </html>
