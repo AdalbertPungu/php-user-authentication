338738ae471c8fda7fb4ae4d14866fe42643b278
Adalbert Pungu <18pw419@esisalama.org>
2022-02-23 02:11:34 +0200

    vérifications des entrées

--- controllers/login.php
+++ controllers/login.php
@@ -5,4 +5,15 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-    
+
+    if (!empty($_SESSION['utilisateurs'][$email]) && $password == $_SESSION['utilisateurs'][$email]['password']) {
+        $_SESSION['connexion'] = $_SESSION['utilisateurs'][$email];
+        header('Location: /profile.php');
+    }elseif ($email == "") {
+        echo "L'email  est vide<br><br>";
+    }elseif ($password == "") {
+        echo "Le mot de passe est vide<br><br>";
+    } else {
+        header('Location: /login.php?error=Coordonnées Incorrectes');
+    }
+
