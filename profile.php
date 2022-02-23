8752de7c0a40eb8eeac7c670cd643e82e197e0d7
Adalbert Pungu <18pw419@esisalama.org>
2022-02-23 02:19:49 +0200

    Add Structure of the html page

---
+++ profile.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    $user = $_SESSION['connexion'];
+
+    if (empty($_SESSION['connexion'])) {
+        header('Location: ./login.php');
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+  </head>
+  <body>
+    
+  </body>
+</html>
